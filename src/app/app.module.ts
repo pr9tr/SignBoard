a9e34cfe47d4198645dc7cbd5af12fbcf31c1c1a
Pranav <pranavtiwary738@gmail.com>
2024-03-08 00:06:40 +0530

    Loader Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoaderComponent,
 
   ],
   imports: [
